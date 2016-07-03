0da861a29413791627626c4495277b6537241e84
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-07-03 15:13:43 -0400

    summarize errors for assignment check

--- c1_intro_programming/scripts/check_a1.sh
+++ c1_intro_programming/scripts/check_a1.sh
@@ -21,19 +21,36 @@ if [ ! -d "$thisAssignment" ]; then
 fi
 
 echo "Looking for files meant to be generated in assignment 1"
-passed=true
+passed="true"
+missing_subdirs=""
+missing_counter=0
 root_subdirs="bin boot dev etc lib media mnt opt sbin srv tmp usr var"
 for subdir in $root_subdirs; do
     target_file="$thisAssignment/$subdir-contents.txt"
 
-    if [ -f $target_file ]; then
-        passed=false
-        echo "Expected to find contents of /$subdir in $target_file, but the file was not found"
-        echo "You can generate this file with the follow commands:"
-        echo
-        echo "    cd /$subdir"
-        echo "    ls -l > $target_file"
-        echo
+    if [ ! -f $target_file ]; then
+        if [ $passed = "true" ]; then
+            echo
+            echo "Expected to find contents of /$subdir in"
+            echo "$target_file, but the file was not found"
+            echo "You can generate this file by entering the following commands:"
+            echo
+            echo "    cd /$subdir"
+            echo "    ls -l > $target_file"
+            echo
+        fi
+        missing_subdirs="$subdir $missing_subdirs"
+        passed="false"
+        ((missing_counter++))
     fi
 done
+
+if [ $passed = "false" ]; then
+    echo "Missing \"*-contents.txt\" files for the following root subdirectories:"
+    echo "$missing_subdirs"
+    echo "There are $missing_counter missing files"
+    echo
+else
+    echo "COMPLETED: Assignment 1"
+fi 
 
