cfa093a6a45cf47ea5716bcc3b79af119433c7fc
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-07-11 00:32:21 +0000

    starting assignment_5 check

---
+++ c1_intro_programming/scripts/check_a5.sh
@@ -0,0 +1,12 @@
+
+check_course_vars_loaded
+
+assignment_index="5"
+thisAssignment="$course_assignments/assignment_$assignment_index"
+check_assignment_started "$assignment_index"
+
+cd $thisAssignment
+passing="true"
+
+
+
