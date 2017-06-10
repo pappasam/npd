a8ad072d6a1e7b2cd04ac0687b16a1ae7b199da4
Rob Marano <robmarano@gmail.com>
2017-06-10 16:40:09 -0400

    Updated Vagrant to use ubuntu/xenial64 since python3 venv had issues on Windows-hosted VMs.

--- c1_intro_programming/scripts/bootstrap.sh
+++ c1_intro_programming/scripts/bootstrap.sh
@@ -2,15 +2,7 @@
 
 apt-get update --fix-missing
 apt-get install vim git zlib1g-dev --assume-yes
-
-cd /home/vagrant/
-sudo wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
-sudo tar -xvf Python-3.6.1.tar.xz
-cd Python-3.6.1
-sudo ./configure
-make
-sudo make install
-
+apt-get install python3 --assume-yes
+apt-get install python3-venv --assume-yes
 apt-get install bpython --assume-yes
 
-###su - vagrant -c "cd /home/vagrant && mkdir dev && cd dev && git clone https://github.com/robmarano/nyu-python.git"
