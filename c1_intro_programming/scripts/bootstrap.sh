226feda989200d6dd30f4ab6d623a03515cdfa28
Rob Marano <robmarano@gmail.com>
2017-06-10 13:48:42 -0400

    Updated bootstrap.sh with latest Python version, and Vagrant image to ubuntu/trusty64

--- c1_intro_programming/scripts/bootstrap.sh
+++ c1_intro_programming/scripts/bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 apt-get update --fix-missing
-apt-get install vim git --assume-yes
+apt-get install vim git zlib1g-dev --assume-yes
 
 cd /home/vagrant/
-sudo wget https://www.python.org/ftp/python/3.4.1/Python-3.4.1.tar.xz
-sudo tar -xvf Python-3.4.1.tar.xz
-cd Python-3.4.1
+sudo wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
+sudo tar -xvf Python-3.6.1.tar.xz
+cd Python-3.6.1
 sudo ./configure
 make
 sudo make install
 
 apt-get install bpython --assume-yes
+
+###su - vagrant -c "cd /home/vagrant && mkdir dev && cd dev && git clone https://github.com/robmarano/nyu-python.git"
